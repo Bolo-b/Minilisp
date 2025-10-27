f5d4419d64482d9ad21e7db759e0adb8fd3545bd
cassandraag <159398314+cassandraag@users.noreply.github.com>
2025-10-27 11:32:54 -0600

    Actualizar Desugar.hs

--- Desugar.hs
+++ Desugar.hs
@@ -9,6 +9,16 @@ data DesuExp = NumD Int
  | Sub DesuExp DesuExp
  | Mult DesuExp DesuExp
  | Div DesuExp DesuExp
+ | Equals DesuExp DesuExp
+ | LessE DesuExp DesuExp
+ | GreatE DesuExp DesuExp
+ | Less DesuExp DesuExp
+ | Great DesuExp DesuExp
+ | Diff DesuExp DesuExp
+ | Not DesuExp
+ | And DesuExp DesuExp
+ | Or DesuExp DesuExp
+ |
  deriving(Show,Eq)
 
 desugar :: Exp -> DesuExp
@@ -26,6 +36,17 @@ desugar (MultP (ParamNumP p1 p2) e1) = Mult (Mult (desugar p1) (desugar p2)) (de
 desugar (MultP e1 e2) = Mult (desugar e1) (desugar e2)
 desugar (DivP (ParamNumP p1 p2) e1) = Div (Div (desugar p1) (desugar p2)) (desugar e1)
 desugar (DivP e1 e2) = Div (desugar e1) (desugar e2)
+
+desugar (EqualsP e1 e2) = Equals how (desugar e1) (desugar e2)
+desugar (LessEP e1 e2) = LessE (desugar e1) (desugar e2)
+desugar (GreatEP e1 e2) = GreatE (desugar e1) (desugar e2)
+desugar (LessP e1 e2) = Less (desugar e1) (desugar e2)
+desugar (GreatP e1 e2) = Great (desugar e1) (desugar e2)
+desugar (DiffP e1 e2) = Diff (desugar e1) (desugar e2)
+
+desugar (BNotP e) = Not (desugar e)
+desugar (BAndP e1 e2) = And (desugar e1) (desugar e2)
+desugar (BOrP e1 e2) = Or (desugar e1) (desugar e2)
 
 
 
