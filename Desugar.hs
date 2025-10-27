e61c1280666ed7b491d8fe56d8db50572a9cac16
cassandraag <159398314+cassandraag@users.noreply.github.com>
2025-10-26 22:20:26 -0600

    Actualizar Desugar.hs

--- Desugar.hs
+++ Desugar.hs
@@ -4,7 +4,7 @@ import Parser (Exp(..))
 data DesuExp = NumD Int
  | Bool Bool
  | Null
- | Id 
+ | Id String
  | Add DesuExp DesuExp
  deriving(Show,Eq)
 
@@ -14,6 +14,15 @@ desugar (NumP n) = (NumD n)
 desugar (BoolP b) = (BoolD b)
 desugar (IdP x) = (Id x)
 desugar NullP = NullD
+
+desugar (AddP (ParamNumP p1 p2) e1) = Add (Add (desugar p1) (desugar p2)) (desugar e1)
+desugar (AddP e1 e2) = Add (desugar e1) (desugar e2)
+desugar (SubP (ParamNumP p1 p2) e1) = Sub (Sub (desugar p1) (desugar p2)) (desugar e1)
+desugar (SubP e1 e2) = Sub (desugar e1) (desugar e2)
+desugar (MultP (ParamNumP p1 p2) e1) = Mult (Mult (desugar p1) (desugar p2)) (desugar e1)
+desugar (MultP e1 e2) = Mult (desugar e1) (desugar e2)
+desugar (DivP (ParamNumP p1 p2) e1) = Div (Div (desugar p1) (desugar p2)) (desugar e1)
+desugar (DivP e1 e2) = Div (desugar e1) (desugar e2)
 
 
 
