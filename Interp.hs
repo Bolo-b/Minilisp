7e716af76cedf5ee6d4bfe2996c95c3195888b71
Oscar <oscarvr07@ciencias.unam.mx>
2025-10-29 15:48:27 -0600

    Casi se completa la funcion sust

--- Interp.hs
+++ Interp.hs
@@ -22,4 +22,20 @@ sust(NumV n) var val= NumP n
 sust(BoolV b) var val= BoolP b 
 sust NullV var val = NullV
 --Asumimos por el momento que se va a definir el IdV
-sust(IdV s) var val =
+sust(IdV s) var val = if s== var then var else (IdV s)
+sust(AddP e1 e2) var val= AddP (sust e1 var val ) (sust e2 var val)
+sust(SubP e1 e2) var val = SubP(sust e1 var val) (sust e2 var val)
+sust(MultP e1 e2) var val =MultP(sust e1 var val) (sust e2 var val)
+sust(DivP e1 e2) var val = DivP(sust e1 var val) (sust e2 var val)
+sust(EqualsP e1 e2) var val = EqualsP(sust e1 var val)(sust e2 var val)
+sust(LessEP e1 e2) var val = LessEP(sust e1 var val)(sust e2 var val)
+sust(GreatP e1 e2) var val =  GreatP sust(sust e1 var val)( sust e2 var val)
+sust(Diff e1 e2) var val = Diff(sust e1 var val)(sust e2 var val)
+sust(BNotP e1) var val = BNotP(e1 var val)
+sust(BAndP e1 e2) var val= BAndP(sust e1 var val)(sust e2 var val)
+sust(BOrP e1 e2)var val= BorP(sust e1 var val)(sust e2 var val)
+--Falta If y aplicacion de funciones
+
+
+
+evalPP:: 
