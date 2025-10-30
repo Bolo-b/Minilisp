e54846aa58f9abde3998b50219ec3396cca7c339
Val <val.cb@ciencias.unam.mx>
2025-10-30 13:38:08 -0600

    interp correccionew

--- Interp.hs
+++ Interp.hs
@@ -16,109 +16,111 @@ busca id [] = Error ("Variable libre: " ++ id)
 busca id ((i,v):xs) = if id == i then v else busca id xs
 
 interp :: DesuExp -> Env -> Value
-interp (NumD n) _ = NumV n
+interp (Num n) _ = NumV n
 interp (Bool b) _ = BoolV b
 interp (Id i) e = busca i e
- esValor::Exp -> Bool
+
+esValor:: DesuExp -> Bool
 esValor (Num _) = True
 esValor (Bool _) = True
 esValor Null = True
 esValor (Lambda _ _)= True
-esValor(Pair e1 e2)= (esValor e1)&&(esValor e2)
+esValor(Pair e1 e2)= esValor e1 && esValor e2
 esValor _= False
-eval::Exp-> Exp
+
+eval::DesuExp -> DesuExp
 eval e
         |esValor e=e
-        |otherwise = eval(Bstep e)
+        |otherwise = eval(bstep e)
 
-Bstep:: Exp-> Exp
-Bstep e | esValor e=e
-Bstep (Id e) = error("Error")
-Bstep (Add (Num n1) (Num))= Num(n1+ n2)
-Bstep (Add (Num n1) e2)= Add (Num n1) (Bstep e2)
-Bstep (Add e1 e2) = Add(Bstep e1)e2
+bstep:: DesuExp -> DesuExp
+bstep e | esValor e=e
+bstep (Id e) = error "Error"
+bstep (Add (Num n1) (Num n2))= Num(n1+ n2)
+bstep (Add (Num n1) e2)= Add (Num n1) (bstep e2)
+bstep (Add e1 e2) = Add(bstep e1)e2
 
-Bstep (Sub (Num n1)(Num n2))= Num(n1-n2)
-Bstep (Sub (Num n1) n2)= Sub (Num n1)(Bstep n2)
-Bstep (Sub e1 e2)= Sub (Bstep e1)e2
+bstep (Sub (Num n1)(Num n2))= Num(n1-n2)
+bstep (Sub (Num n1) n2)= Sub (Num n1)(bstep n2)
+bstep (Sub e1 e2)= Sub (bstep e1)e2
 
-Bstep (Mult (Num n1)(Num n2))= Num(n1*n2)
-Bstep (Mult (Num n1) n2)= Mult (Num n1)(Bstep n2)
-Bstep (Mult e1 e2)= Mult (Bstep e1)e2
+bstep (Mult (Num n1)(Num n2))= Num(n1*n2)
+bstep (Mult (Num n1) n2)= Mult (Num n1)(bstep n2)
+bstep (Mult e1 e2)= Mult (bstep e1)e2
 
-BStep (Div (Num n1) (Num 0)) = error"No se puede dividir por cero"
-BStep (Div (Num n1) (Num n2)) = Num (n1 `Div` n2)
-BStep (Div (Num n1) e2) = Div (Num n1) (BStep e2)
-BStep (Div e1 e2) = Div (BStep e1) e2
+bstep (Div (Num n1) (Num 0)) = error"No se puede dividir por cero"
+bstep (Div (Num n1) (Num n2)) = Num (n1 `Div` n2)
+bstep (Div (Num n1) e2) = Div (Num n1) (bstep e2)
+bstep (Div e1 e2) = Div (bstep e1) e2
 
-Bstep (Sub (Num n1)(Num n2))= Num(n1-n2)
-Bstep (Sub (Num n1) n2)= Sub (Num n1)(Bstep n2)
-Bstep (Sub e1 e2)= Sub (Bstep e1)e2
+bstep (Sub (Num n1)(Num n2))= Num(n1-n2)
+bstep (Sub (Num n1) n2)= Sub (Num n1)(bstep n2)
+bstep (Sub e1 e2)= Sub (bstep e1)e2
 
-Bstep (Equals e1 e2)
-        |not (esValor e1)= Equals(Bstep e1) e2
-        | esValor && not (esValor e2)= Equals e1(Bstep e2)
+bstep (Equals e1 e2)
+        |not (esValor e1)= Equals(bstep e1) e2
+        | esValor && not (esValor e2)= Equals e1(bstep e2)
         | otherwise = case (e1, e2) of
                 (Num n1, Num n2)-> Bool(n1==n2)
-Bstep (LessE e1 e2)
-        |not (esValor e1)= LessE(Bstep e1) e2
-        | esValor && not (esValor e2)=LessE e1(Bstep e2)
+bstep (LessE e1 e2)
+        |not (esValor e1)= LessE(bstep e1) e2
+        | esValor && not (esValor e2)=LessE e1(bstep e2)
         | otherwise = case (e1, e2) of
                 (Num n1, Num n2)-> Bool(n1<n2)
-Bstep (GreatE e1 e2)
-        |not (esValor e1)= GreatE(Bstep e1) e2
-        | esValor && not (esValor e2)= GreatE e1(Bstep e2)
+bstep (GreatE e1 e2)
+        |not (esValor e1)= GreatE(bstep e1) e2
+        | esValor && not (esValor e2)= GreatE e1(bstep e2)
         | otherwise = case (e1, e2) of
                 (Num n1, Num n2)-> Bool(n1>n2)
-Bstep (Less e1 e2)
-        |not (esValor e1)= Less(Bstep e1) e2
-        | esValor && not (esValor e2)= Less e1(Bstep e2)
+bstep (Less e1 e2)
+        |not (esValor e1)= Less(bstep e1) e2
+        | esValor && not (esValor e2)= Less e1(bstep e2)
         | otherwise = case (e1, e2) of
                 (Num n1, Num n2)-> Bool(n1<=n2)
-Bstep (Great e1 e2)
-        |not (esValor e1)= Great(Bstep e1) e2
-        | esValor && not (esValor e2)= Great e1(Bstep e2)
+bstep (Great e1 e2)
+        |not (esValor e1)= Great(bstep e1) e2
+        | esValor && not (esValor e2)= Great e1(bstep e2)
         | otherwise = case (e1, e2) of
                 (Num n1, Num n2)-> Bool(n1>=n2)
-Bstep (DiffD e1 e2)
-        |not (esValor e1)= DiffD(Bstep e1) e2
-        | esValor && not (esValor e2)= DiffD e1(Bstep e2)
+bstep (DiffD e1 e2)
+        |not (esValor e1)= DiffD(bstep e1) e2
+        | esValor && not (esValor e2)= DiffD e1(bstep e2)
         | otherwise = case (e1, e2) of
-                (Num n1, Num n2)-> Bool(n1!=n2)
-Bstep(Not(Bool b))= Bool(not b)
-Bstep(Not e)= Not(Bstep e)
-Bstep(And (Bool False)_)= Bool False
-Bstep(And (Bool True)e2)= e2
-Bstep(And e1 e2)= And (Bstep e1) e2
-Bstep(Or(Bool True)_)= Or True
-Bstep(Or (Bool True)e2)= e2
-Bstep(Or e1 e2)= Or(Bstep e1) e2
-Bstep(If(Bool True)t _)= t
-Bstep(If(Bool False)_ e)= e
-BStep(If c t e)= If(Bstep c) t e
-Bstep(App (Lambda p b)a)
+                (Num n1, Num n2)-> Bool(n1 != n2)
+bstep(Not(Bool b))= Bool(not b)
+bstep(Not e)= Not(bstep e)
+bstep(And (Bool False)_)= Bool False
+bstep(And (Bool True)e2)= e2
+bstep(And e1 e2)= And (bstep e1) e2
+bstep(Or(Bool True)_)= Or True
+bstep(Or (Bool True)e2)= e2
+bstep(Or e1 e2)= Or(bstep e1) e2
+bstep(If(Bool True)t _)= t
+bstep(If(Bool False)_ e)= e
+bstep(If c t e)= If(bstep c) t e
+bstep(App (Lambda p b)a)
         |esValor a= sust b p a
-        |otherwise =App(Lambda p b) (Bstep a)
-Bstep(App f a)= App(BStep f)a
-Bstep(Pair e1 e2)
-        |not (esValor e1)= Pair(esValor e1 )e2
-        |esValor e1 && not (esValor e2 )= Pair e1 (Bstep e2)
-        |otherwise =e
-Bstep(Fst(Pair v1 v2)) | esValor(Pair v1 v2 )=v1
-Bstep(Fst e)= Fst(Bstep e)
-Bstep(Snd(Pair v1 v2)) | esValor(Pairv1 v2)= v2
-Bstep(Snd e )= Snd(BStep e)
+        |otherwise =App(Lambda p b) (bstep a)
+bstep(App f a)= App(bstep f)a
+bstep(Pair e1 e2)
+        |not (esValor e1) = Pair (bstep e1 ) e2
+        |esValor e1 && not (esValor e2 ) = Pair e1 (bstep e2)
+        |otherwise = (Pair e1 e2)
+bstep(Fst(Pair v1 v2)) | esValor(Pair v1 v2 )=v1
+bstep(Fst e)= Fst(bstep e)
+bstep(Snd(Pair v1 v2)) | esValor(Pair v2)= v2
+bstep(Snd e )= Snd(bstep e)
 --Por si hay un error
-Bstep e = error("Bstep fallo en la implementacion de"++ show e)
+bstep e = error("bstep fallo en la implementacion de"++ show e)
 
 
 --Recibe una expresion, un identificador, una expresion y devuelve una expresion
 --Entonces sustituye en este arbol llamado tal por este valor
-sust :: Exp-> String-> Exp-> Exp
+sust :: DesuExp -> String -> Value -> DesuExp
 sust(Num n) _  _= Num n
 sust(Bool b) _  _= Bool b 
 sust Null _  _ = Null
-sust(Id s) var val = if s == var then val else (Id s)
+sust(Id s) var val = if s == var then val else Id s
 sust(Add e1 e2) var val= Add (sust e1 var val ) (sust e2 var val)
 sust(Sub e1 e2) var val = Sub(sust e1 var val) (sust e2 var val)
 sust(Mult e1 e2) var val =Mult(sust e1 var val) (sust e2 var val)
@@ -130,7 +132,7 @@ sust(DiffD e1 e2) var val = DiffD(sust e1 var val)(sust e2 var val)
 sust(Not e1) var val = Not(e1 var val)
 sust(And e1 e2) var val= And(sust e1 var val)(sust e2 var val)
 sust(Or e1 e2) var val= Or(sust e1 var val)(sust e2 var val)
-sust(Sub e) var val = Sub (sust e var val)
+sust(Sub e1 e2) var val = Sub (sust e1 var val) (sust e2 var val)
 sust(If c t e) var val= If (sust c var val) (sust t var val) (sust e var val)
 sust(App f a) var val= Add (sust f var val) (sust a var val)
 sust(Pair e1 e2 ) var val = Pair(sust e1 var val) (sust e2 var val)
