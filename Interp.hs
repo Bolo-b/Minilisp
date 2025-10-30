27b48b550fa8108387d1a4c855f94bfdce54147d
Oscar <oscarvr07@ciencias.unam.mx>
2025-10-29 20:20:20 -0600

    Se avanzo con la funcion sust pero le falta y todavia no es compilable

--- Interp.hs
+++ Interp.hs
@@ -5,7 +5,22 @@ type Env = [(String, ASAValues)]
 data Value = NumV Int
         | BoolV Bool
         | ClosureV [Value]
-        | NullV 
+        | NullV
+        | IdP String
+        | AddP Exp Exp
+        | SubP Exp Exp
+        | MultP Exp Exp
+        | DivP Exp Exp
+        | EqualsP Exp Exp
+        | LessEP Exp Exp
+        | GreatP Exp Exp
+        | Diff Exp Exp
+        | BNotP Exp
+        | BorP Exp Exp
+        | IfP Exp Exp Exp
+      --| LetP String Exp Exp
+      --| LambdaP String Exp
+        | AppP Exp Exp
         | Error String
         deriving(Show,Eq)
 busca :: String -> DS -> Value
@@ -18,11 +33,10 @@ interp (Id i) ds = busca i ds
 --Recibe una expresion, un identificador, una expresion y devuelve una expresion
 --Entonces sustituye en este arbol llamado tal por este valor
 sust :: Exp-> String-> Exp-> Exp
-sust(NumV n) var val= NumP n
-sust(BoolV b) var val= BoolP b 
-sust NullV var val = NullV
---Asumimos por el momento que se va a definir el IdV
-sust(IdV s) var val = if s== var then var else (IdV s)
+sust(NumV n) _  _= NumP n
+sust(BoolV b) _  _= BoolP b 
+sust NullV _  _ = NullV
+sust(IdP s) var val = if s == var then val else (IdP s)
 sust(AddP e1 e2) var val= AddP (sust e1 var val ) (sust e2 var val)
 sust(SubP e1 e2) var val = SubP(sust e1 var val) (sust e2 var val)
 sust(MultP e1 e2) var val =MultP(sust e1 var val) (sust e2 var val)
@@ -33,8 +47,20 @@ sust(GreatP e1 e2) var val =  GreatP sust(sust e1 var val)( sust e2 var val)
 sust(Diff e1 e2) var val = Diff(sust e1 var val)(sust e2 var val)
 sust(BNotP e1) var val = BNotP(e1 var val)
 sust(BAndP e1 e2) var val= BAndP(sust e1 var val)(sust e2 var val)
-sust(BOrP e1 e2)var val= BorP(sust e1 var val)(sust e2 var val)
---Falta If y aplicacion de funciones
+sust(BOrP e1 e2) var val= BorP(sust e1 var val)(sust e2 var val)
+sust(SubP e) var val = SubP (sust e var val)
+sust(IfP c t e) var val= IfP (sust c var val) (sust t var val) (sust e var val)
+sust(AppP f a) var val= AddP (sust f var val) (sust a var val)
+sust(PairP e1 e2 ) var val = PairP(sust e1 var val) (sust e2 var val)
+sust(FstP e) var val = FstP (sust e var val)
+sust(SndP e) var val = SndP (sust e var val)
+sust(ListP exps) var val = ListP --???
+sust(HeadLP e) var val = HeadLP(sust e var val)
+sust(TailLP e) var val = TailLP(sust e var val)
+--Falta Lambda,FunP,FunRecP y Lista
+
+--Para ver que error Sale
+sust e _ _ = error ("Fallo o falta en la implementacion de:"++ show e)
 
 
 
