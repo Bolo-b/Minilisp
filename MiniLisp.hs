257e2d38721c663ea3ea504417a37856988d7054
cassandraag <159398314+cassandraag@users.noreply.github.com>
2025-11-03 19:32:14 -0600

    Refactor saca function and add run functionya casi

--- MiniLisp.hs
+++ MiniLisp.hs
@@ -1,9 +1,20 @@
 module MiniLisp where
-import Interp
 
-saca :: ASAFinal -> String
-saca (NumF n) = n
-saca (BoolF b)
-  | b == True = "#t"
-  | otherwise = "#f"
-saca (ClosureV p c e) = "lambda(" + p + " " + c + ")"
+import Interp(eval)
+import Lexer(lexer)
+import Desugar(desugar, DesuExp(..))
+import Parser(parse)
+
+
+run:: String-> String
+run s = saca (eval(desugar(parse(lexer s))))
+
+saca :: DesuExp -> String
+saca (Num n) = show n
+saca (Bool b) = if b then "#t" else "#f"
+saca Null = "[]"
+saca (Pair v1 v2) = "(" ++ (saca v1) ++ ", " ++ (saca v2) ++ ")"
+saca (Lambda _ _) = "<funcion>"
+saca (Fix e) = "<fix>"
+saca (Id s) = s
+saca e = "(Expresión compleja: " ++ (show e) ++ ")"
